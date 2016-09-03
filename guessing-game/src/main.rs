382cc0cb1beb1cbb1c3382202d9f7cb3de86c7a1
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-09-03 20:00:58 +0100

    Generate secret number to be guessed.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess a number!");
+
+    // Draw a number from 1 to 100.
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret);
 
     println!("Please input your guess.");
 
