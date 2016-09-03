0dc1ef54eef20b86673ccc1d290046413e555411
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-09-03 23:41:35 +0100

    Loop guessing until secret is found.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -13,18 +13,25 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line!");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed {}", guess);
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too large!"),
-        Ordering::Equal   => println!("You win!"),
+        match guess.cmp(&secret) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
