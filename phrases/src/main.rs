e0b0d0e8d4b42a713b86f7ebf40bccb169e4b8f9
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-09-22 11:45:03 +0100

    Bring japanese functions to module scope.

--- phrases/src/main.rs
+++ phrases/src/main.rs
@@ -1,10 +1,11 @@
 extern crate phrases;
 
 use phrases::english::{greetings,farewells};
+use phrases::japanese;
 
 fn main() {
     println!("Hello in English is {}", greetings::hello());
     println!("Goodbye in English is {}", farewells::goodbye());
-    println!("Hello in Japanese is {}", phrases::japanese::greetings::hello());
-    println!("Goodbye in Japanese is {}", phrases::japanese::farewells::goodbye());
+    println!("Hello in Japanese is {}", japanese::hello());
+    println!("Goodbye in Japanese is {}", japanese::goodbye());
 }
