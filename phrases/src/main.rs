df7d3da89de5a62aba0b4f33e17511effda2d922
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-09-22 11:42:38 +0100

    Bring english module functions to main scope.

--- phrases/src/main.rs
+++ phrases/src/main.rs
@@ -1,8 +1,10 @@
 extern crate phrases;
 
+use phrases::english::{greetings,farewells};
+
 fn main() {
-    println!("Hello in English is {}", phrases::english::greetings::hello());
-    println!("Goodbye in English is {}", phrases::english::farewells::goodbye());
+    println!("Hello in English is {}", greetings::hello());
+    println!("Goodbye in English is {}", farewells::goodbye());
     println!("Hello in Japanese is {}", phrases::japanese::greetings::hello());
     println!("Goodbye in Japanese is {}", phrases::japanese::farewells::goodbye());
 }
