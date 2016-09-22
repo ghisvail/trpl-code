6f4f2a65a8a0d18b342be340919af1a34fa86a69
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-09-22 11:51:07 +0100

    Demonstrate usage of aliases from modules.

--- phrases/src/main.rs
+++ phrases/src/main.rs
@@ -1,11 +1,11 @@
 extern crate phrases as sayings;
 
-use sayings::english::{greetings,farewells};
-use sayings::japanese;
+use sayings::english::{greetings as en_greetings, farewells as en_farewells};
+use sayings::japanese as jp;
 
 fn main() {
-    println!("Hello in English is {}", greetings::hello());
-    println!("Goodbye in English is {}", farewells::goodbye());
-    println!("Hello in Japanese is {}", japanese::hello());
-    println!("Goodbye in Japanese is {}", japanese::goodbye());
+    println!("Hello in English is {}", en_greetings::hello());
+    println!("Goodbye in English is {}", en_farewells::goodbye());
+    println!("Hello in Japanese is {}", jp::hello());
+    println!("Goodbye in Japanese is {}", jp::goodbye());
 }
