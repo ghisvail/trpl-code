f497789feb46894244e2c9f5656ccc735a83704a
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-10-01 20:33:10 +0100

    Improve file-double error handling.
    - Use a Result type and handle error in main.
    - Convert all errors to string type for printing.
    - Use combination of map_err / and_then / map to keep the logic flow.

--- print-extension/file-double/src/main.rs
+++ print-extension/file-double/src/main.rs
@@ -2,15 +2,25 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-fn file_double<P: AsRef<Path>>(file_path: P) -> i32 {
-    let mut file = File::open(file_path).unwrap();  // error 1
-    let mut content = String::new();
-    file.read_to_string(&mut content).unwrap();     // error 2
-    let n: i32 = content.trim().parse().unwrap();     // error 3
-    2 * n
+fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
+    File::open(file_path)
+        .map_err(|err| err.to_string())
+        .and_then(|mut file| {
+            let mut content = String::new();
+            file.read_to_string(&mut content)
+                .map_err(|err| err.to_string())
+                .map(|_| content)
+        })
+        .and_then(|content| {
+            content.trim().parse::<i32>()
+                .map_err(|err| err.to_string())
+        })
+        .map(|n| 2 * n)
 }
 
 fn main() {
-    let doubled = file_double("double.me");
-    println!("{}", doubled);
+    match file_double("double.me") {
+        Ok(n) => println!("{}", n),
+        Err(err) => println!("Error: {}", err),
+    }
 }
