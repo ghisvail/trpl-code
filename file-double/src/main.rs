e53cad90cdfc9dd114e210ec21d2f046ad22df55
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-10-04 11:35:17 +0100

    Finally get rid of map_err via impl of conversion.

--- file-double/src/main.rs
+++ file-double/src/main.rs
@@ -13,11 +13,23 @@ enum CliError {
     Parse(num::ParseIntError),
 }
 
+impl From<io::Error> for CliError {
+    fn from(err: io::Error) -> CliError {
+        CliError::Io(err)
+    }
+}
+
+impl From<num::ParseIntError> for CliError {
+    fn from(err: num::ParseIntError) -> CliError {
+        CliError::Parse(err)
+    }
+}
+
 fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, CliError> {
-    let mut file = try!(File::open(file_path).map_err(CliError::Io));
+    let mut file = try!(File::open(file_path));
     let mut content = String::new();
-    try!(file.read_to_string(&mut content).map_err(CliError::Io));
-    let n = try!(content.trim().parse::<i32>().map_err(CliError::Parse));
+    try!(file.read_to_string(&mut content));
+    let n = try!(content.trim().parse::<i32>());
     Ok(2 * n)
 }
 
