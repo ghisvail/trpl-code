e58eb39bed26c3dc235050b8a5c8f97592a1c650
Ghislain Antony Vaillant <ghisvail@gmail.com>
2016-10-04 11:25:30 +0100

    Use custom error type.

--- file-double/src/main.rs
+++ file-double/src/main.rs
@@ -2,17 +2,28 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
-    let mut file = try!(File::open(file_path).map_err(|e| e.to_string()));
+use std::io;
+use std::num;
+
+// We derive `Debug` because all types should probably derive `Debug`.
+// This gives us a reasonable human readable description of `CliError` values.
+#[derive(Debug)]
+enum CliError {
+    Io(io::Error),
+    Parse(num::ParseIntError),
+}
+
+fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, CliError> {
+    let mut file = try!(File::open(file_path).map_err(CliError::Io));
     let mut content = String::new();
-    try!(file.read_to_string(&mut content).map_err(|e| e.to_string()));
-    let n = try!(content.trim().parse::<i32>().map_err(|e| e.to_string()));
+    try!(file.read_to_string(&mut content).map_err(CliError::Io));
+    let n = try!(content.trim().parse::<i32>().map_err(CliError::Parse));
     Ok(2 * n)
 }
 
 fn main() {
     match file_double("double.me") {
         Ok(n) => println!("{}", n),
-        Err(err) => println!("Error: {}", err),
+        Err(err) => println!("Error: {:?}", err),
     }
 }
